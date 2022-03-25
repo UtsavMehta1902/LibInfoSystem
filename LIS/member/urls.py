cc739882d8afdec2fe445b543f35dce8a8343e27
jena-shreyas <jenashreyas@gmail.com>
2022-03-25 11:08:06 +0530

    modified html files, placed under separate folders within templates

--- LIS/member/urls.py
+++ LIS/member/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("registration/", views.member_registration, name="member_registration"),
+    path("member_registration/", views.member_registration, name="member_registration"),
     path("login/", views.member_login, name="member_login"),
     path("profile/", views.profile, name="profile"),
 ]
