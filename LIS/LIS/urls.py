aca6130cec7b8a0982a52a5d5e3544e3f0d8d209
UtsavMehta1902 <utsavmehta2002@yahoo.com>
2022-03-25 17:02:18 +0530

    Added basic home page.

--- LIS/LIS/urls.py
+++ LIS/LIS/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('member/', include('member.urls')),
     path('staff/',include('staff.urls')),
+    path('', include('book.urls')),
 ]
