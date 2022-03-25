01fdee76ebe003b05bccad080613bbeb63f14b39
jena-shreyas <jenashreyas@gmail.com>
2022-03-25 11:54:19 +0530

    updated views.py of staff

--- LIS/staff/views.py
+++ LIS/staff/views.py
@@ -25,7 +25,7 @@ def staff_registration(request):
 
         if password != confirm_password:
             passnotmatch = True
-            return render(request, "staff_registration.html", {'passnotmatch':passnotmatch})
+            return render(request, "staff/staff_registration.html", {'passnotmatch':passnotmatch})
 
         user = User.objects.create_user(username=username, email=email, password=password,first_name=first_name, last_name=last_name)
         staff = Staff.objects.create(user=user)
@@ -33,8 +33,8 @@ def staff_registration(request):
         user.save()
         staff.save()
         alert = True
-        return render(request, "staff_registration.html", {'alert':alert})
-    return render(request, "staff_registration.html")
+        return render(request, "staff/staff_registration.html", {'alert':alert})
+    return render(request, "staff/staff_registration.html")
 
 
 @login_required(login_url = '/admin_login')
@@ -49,34 +49,34 @@ def add_book(request):
         books = Book.objects.create(name=name, author=author, isbn=isbn, category=category)
         books.save()
         alert = True
-        return render(request, "add_book.html", {'alert':alert})
-    return render(request, "add_book.html")
+        return render(request, "staff/add_book.html", {'alert':alert})
+    return render(request, "staff/add_book.html")
 
 
 @login_required(login_url = '/admin_login')
 def view_books(request):
     books = Book.objects.all()
-    return render(request, "view_books.html", {'books':books})
+    return render(request, "staff/view_books.html", {'books':books})
 
 
 @login_required(login_url = '/admin_login')
 def view_members(request):
     members = Member.objects.all()
-    return render(request, "view_members.html", {'members':members})
+    return render(request, "staff/view_members.html", {'members':members})
 
 
 @login_required(login_url = '/admin_login')
 def delete_book(request, myid):
     books = Book.objects.filter(id=myid)
     books.delete()
-    return redirect("/view_books")
+    return redirect("staff/view_books")
 
 
 @login_required(login_url = '/admin_login')
 def delete_member(request, myid):
     members = Member.objects.filter(id=myid)
     members.delete()
-    return redirect("/view_members")
+    return redirect("staff/view_members")
 
 
 def admin_login(request):
@@ -88,17 +88,17 @@ def admin_login(request):
         if user is not None:
             login(request, user)
             if request.user.is_superuser:
-                return redirect("/add_book")
+                return redirect("staff/add_book")
             else:
                 return HttpResponse("You are not an admin.")
         else:
             alert = True
-            return render(request, "admin_login.html", {'alert':alert})
-    return render(request, "admin_login.html")
+            return render(request, "staff/admin_login.html", {'alert':alert})
+    return render(request, "staff/admin_login.html")
 
 def Logout(request):
     logout(request)
-    return redirect ("/")
+    return redirect ("/staff/")
 
 
     
