66b8fd33fb72c0d325823489b47a0bbf250e75c4
PranavNyati <pranavnyati26@gmail.com>
2022-03-27 19:03:52 +0530

    added insti_id for login

--- LIS/member/views.py
+++ LIS/member/views.py
@@ -4,22 +4,23 @@ from .models import *
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-UG_cnt=0
-PG_cnt=0
-RS_cnt=0
-FAC_cnt=0
+# UG_cnt=0
+# PG_cnt=0
+# RS_cnt=0
+# FAC_cnt=0
 
 def member_home_page(request):
     return render(request, "member/home.html")
 
 def member_registration(request):
     if request.method == "POST":
-        global UG_cnt, PG_cnt, RS_cnt, FAC_cnt
+        # global UG_cnt, PG_cnt, RS_cnt, FAC_cnt
 
         member_type = request.POST['member_type']
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
         email = request.POST['email']
+        insti_id = request.POST['insti_id']
         password = request.POST['password']
         confirm_password = request.POST['confirm_password']
         
@@ -28,23 +29,23 @@ def member_registration(request):
         duration=0
 
         if member_type == "UG":
-            username= "UG_" + str(UG_cnt)
-            UG_cnt+=1
+            username= "UG_" + str(insti_id)
+            # UG_cnt+=1
             limit=2
             duration=1
         elif member_type == "PG":
-            username= "PG_" + str(PG_cnt)
-            PG_cnt+=1
+            username= "PG_" + str(insti_id)
+            # PG_cnt+=1
             limit=4
             duration=1
         elif member_type == "RS":
-            username= "RS_" + str(RS_cnt)
-            RS_cnt+=1
+            username= "RS_" + str(insti_id)
+            # RS_cnt+=1
             limit=6
             duration=3
         elif member_type == "FAC":
-            username= "FAC_" + str(FAC_cnt)
-            FAC_cnt+=1
+            username= "FAC_" + str(insti_id)
+            # FAC_cnt+=1
             limit=10
             duration=6
 
@@ -52,7 +53,7 @@ def member_registration(request):
             passnotmatch = True
             return render(request, "member/registration.html", {'passnotmatch':passnotmatch})
 
-        user = User.objects.create_user(username=username, email=email, password=password,first_name=first_name, last_name=last_name)
+        user = User.objects.create_user(username=username, email=email, password=password,first_name=first_name, last_name=last_name, insti_id=insti_id )
         member = Member.objects.create(user=user, book_limit=limit, book_duration=duration)
         return redirect('/member/login')
         user.save()
